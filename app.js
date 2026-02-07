a3ad0597315dff91e387cca5673eaba3e46fe9a5
subratsingh01 <subratprivate777@gmail.com>
2026-02-07 15:45:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 import express from "express";
-import cors from 'cors'
-// import route from './text.js'
-const app = express()
+import cors from "cors";
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(
-    cors({
-      origin: "*",
-      credentials: false,
-      allowedHeaders: ["Content-Type", "Authorization"],
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    })
+  cors({
+    origin: "*",
+    credentials: false,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
 );
 
-app.get('/', (req, res) => {
-    res.send("server is listening")
-})
+app.get("/", (req, res) => {
+  res.send("server is listening");
+});
 
 app.get("/text", (req, res) => {
-    const text = req.params.data;
-    console.log(data);
-})
+  const { data } = req.query;   // 👈 query param
+  console.log(data);
 
-app.listen(3000, () => console.log("listening on port 3000"))
+  res.json({
+    message: "query param received",
+    data,
+  });
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
