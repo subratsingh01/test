2c2ab33a0b1a19c6b0d08940b73ec8cdbd962717
subratsingh01 <subratprivate777@gmail.com>
2026-02-07 15:18:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from 'cors'
+import {text} from './text.js'
+const app = express()
+
+app.use(express.json())
+app.use(
+    cors({
+      origin: "*",
+      credentials: false,
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    })
+);
+
+app.get('/', (req, res) => {
+    res.send(`<h1>listening</h1>`)
+})
+
+app.use('/v1', text);
+
+app.listen(3000, () => console.log("listening on port 3000"))
